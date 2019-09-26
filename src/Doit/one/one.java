eb8236b79a7a94fdeb624b46cbef368352668529
KIMINJOONG <ijboym216@gmail.com>
2019-09-26 23:01:51 +0900

    init

--- src/Doit/one/one.java
+++ src/Doit/one/one.java
@@ -1,7 +1,10 @@
 package Doit.one;
 
+import java.util.Scanner;
+
 public class one {
     public static void main(String[] args) {
-
+        Scanner stdIn = new Scanner(System.in);
+        stdIn.nextInt();
     }
 }

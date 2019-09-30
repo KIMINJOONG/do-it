cac4f5f4afb1eb031227b610b76ec7dabf98395f
KIMINJOONG <ijboym216@gmail.com>
2019-09-30 09:31:38 +0900

    solve question

--- src/Doit/chap01/question/q1.java
+++ src/Doit/chap01/question/q1.java
@@ -16,8 +16,39 @@ public class q1 {
         return max;
     }
 
+    static int min3(int a, int b, int c) {
+        int min = a;
+        if(min > b) {
+            min = b;
+        }
+        if(min > c) {
+            min = c;
+        }
+        return min;
+    }
+
+    static int min4(int a, int b, int c, int d) {
+        int min = a;
+        if(min > b) {
+            min = b;
+        }
+        if(min > c) {
+            min = c;
+        }
+        if(min > d) {
+            min = d;
+        }
+        return min;
+    }
+
     public static void main(String[] args) {
         int max = max4(4, 6, 3, 1);
         System.out.println(max);
+
+        int min = min3(7, 2, 5);
+        System.out.println("q2 세값의 최솟값을 구하는 min3 : " + min);
+
+        int min2 = min4(3, 6, 5, 2);
+        System.out.println("q3 네값의 최솟값을 구하는 min4 : " + min2);
     }
 }

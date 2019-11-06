dbd3b2560adbf673126ff4e9e934d3849676cc63
KIMINJOONG <ijboym216@gmail.com>
2019-11-06 21:32:34 +0900

    fix q4

--- src/Doit/chap02/question/q4.java
+++ src/Doit/chap02/question/q4.java
@@ -1,5 +1,7 @@
 package Doit.chap02.question;
 
+import java.util.Scanner;
+
 public class q4 {
     static void copy(int[] a, int[] b) {
         for(int i = 0; i < b.length; i++) {
@@ -8,15 +10,20 @@ public class q4 {
     }
 
     public static void main(String[] args) {
+        System.out.print("배열의 길이를 입력해주세요.");
+        Scanner stdIn = new Scanner(System.in);
+        int[] b = new int[stdIn.nextInt()];
 
-        int[] b = {1,2,3,4,5};
+        for(int i = 0; i < b.length; i++) {
+            System.out.print("배열의 " + i + "번째 요소의 값을 입력해주세요.");
+            b[i] = stdIn.nextInt();
+        }
         int[] a = new int[b.length];
         copy(a, b);
 
         for(int i = 0; i < a.length; i++) {
             System.out.println(a[i]);
         }
-
     }
 }
 

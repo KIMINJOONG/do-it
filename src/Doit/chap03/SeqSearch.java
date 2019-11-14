efe43a13cdd0a975d48c17c3632801e008153c11
KIMINJOONG <ijboym216@gmail.com>
2019-11-14 10:03:37 +0900

    white, for seqSearch

--- src/Doit/chap03/SeqSearch.java
+++ src/Doit/chap03/SeqSearch.java
@@ -14,6 +14,14 @@ public class SeqSearch {
         }
     }
 
+    static int forSeqSearch(int[] a, int n, int key) {
+        for(int i = 0; i < n; i++)
+            if(a[i] == key)
+                return i;
+            return -1;
+
+    }
+
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
 

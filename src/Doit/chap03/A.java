781cf66710b49bc4a456b3962434ec489467f272
KIMINJOONG <ijboym216@gmail.com>
2019-11-26 23:22:56 +0900

    chap 3-7

---
+++ src/Doit/chap03/A.java
@@ -0,0 +1,9 @@
+package Doit.chap03;
+
+public class A implements Comparator<A> {
+
+    @Override
+    public int compare(A o1, A o2) {
+        return 0;
+    }
+}
---
+++ src/Doit/chap03/X.java
@@ -0,0 +1,13 @@
+package Doit.chap03;
+
+public class X {
+    public static final Comparator<T> COMPARATOR = new Comp();
+
+    private static class Comp implements Comparator<T> {
+
+        @Override
+        public int compare(T o1, T o2) {
+            return 0;
+        }
+    }
+}

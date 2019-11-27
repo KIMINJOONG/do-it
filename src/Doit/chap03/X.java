f562699cbbc1af34a6106956a4f1ed47e3b1cca7
KIMINJOONG <ijboym216@gmail.com>
2019-11-27 19:50:36 +0900

    GenericClassTester

--- src/Doit/chap03/X.java
+++ src/Doit/chap03/X.java
@@ -1,13 +1,14 @@
-package Doit.chap03;
-
-public class X {
-    public static final Comparator<T> COMPARATOR = new Comp();
-
-    private static class Comp implements Comparator<T> {
-
-        @Override
-        public int compare(T o1, T o2) {
-            return 0;
-        }
-    }
-}
+//package Doit.chap03;
+//
+//
+//public class X {
+//    public static final java.util.Comparator<T> COMPARATOR = new Comp();
+//
+//    private static class Comp implements Comparator<T> {
+//
+//        @Override
+//        public int compare(T o1, T o2) {
+//            return 0;
+//        }
+//    }
+//}
